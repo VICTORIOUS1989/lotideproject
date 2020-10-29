1c0574a6373381d8ab4af044bf34fc06394c5d67
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-10-29 19:50:46 +0000

    position of letters

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i= 0; i < sentence.length ; i++) {
+
+    if(!results[sentence[i]] )                  
+    results[sentence[i]] = [i];
+    else {
+      results[sentence[i]].push(i);
+    }
+  }
+           
+
+return results;    
+};
+
+
+
+console.log(letterPositions("abbadgffdd"));
