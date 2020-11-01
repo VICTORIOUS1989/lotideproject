038b86f2347274610c29b0b78a9564f50693f5a9
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-01 19:06:23 +0000

    export the function and delete the test

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
+
+const assertEqual= require('./assertEqual');
 let head = function (array) {
   if ( array.length > 0 ) return array[0];
   else return undefined ;
 }
 
+module.exports= head;
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  else return `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`;
-  };
-
-
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
