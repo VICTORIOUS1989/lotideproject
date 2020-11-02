b5e6496b7ad9d5fb26390c674bbd87e76f2ebcbe
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-02 01:53:06 +0000

    count letter

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+
+
+  function countLetter(str) {
+    let count = 0;
+    let object ={};
+    let temp = str ;
+    let obj ={};
+
+    for(let s of str)
+    if(!obj[s]) obj[s] = 1;
+    else obj[s] = obj[s]  + 1;
+    return obj;
+  
+  }
+  
+
+
+  function countLetters(str) {
+    let count = 0;
+    let object ={};
+    let temp = str ;
+    let obj ={};
+
+    for(let s of str){
+        for (let item of str){
+          
+        }
+    }
+    
+  
+  }
+  
+
+console.log(countLetters("dfdsfdf"));
+
+console.log(countLetter("dfdsfdf"));
+
