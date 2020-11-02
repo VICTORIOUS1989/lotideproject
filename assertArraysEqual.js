8dc256e5908f5846f747d3480f5e24cf0b785ecd
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-02 01:27:06 +0000

    assert array

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,12 @@
 
-const eqArrays = function (array1, array2)  {
-  let bool;
-  if (array1.length !== array2.length) return false;
-  else {
-    for (let i = 0 ; i < array1.length ; i++  ) {
-      if (array1[i] !== array2[i]) {
-        bool = false ;
-        break;
-      }
-      else bool = true ;
-      }
-      
-  } 
-  return bool;
-}
-
+const eqArrays = require ('../eqArrays');
 
 
 function assertArraysEqual(array1 , array2) {
   if (eqArrays(array1 , array2) === true ) return "✅✅✅ Assertion Passed: array1 === array2";
   else  return "🛑🛑🛑 Assertion failed: array1 !== array2";
 }
+
+module.export=assertArraysEqual;
 
 
