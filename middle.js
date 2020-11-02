21c03c92eadd5f96270bbad6334a283250969ecc
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-02 01:32:47 +0000

    middle test function

--- middle.js
+++ middle.js
@@ -20,8 +20,4 @@ const middle = function (array) {
 
 }
 
-
-console.log(middle([1, 2, 3, 4]) )// => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+module.export= middle;
