737b6e46e04b273294f916a0f09fe1b905134610
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-02 01:47:40 +0000

    tailTest with mocka and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,22 @@
+const { assert } = require("chai");
+
 const assertEqual = require ('../assertEqual');
 const tail = require ('../tail');
+const result = tail(["Hello", "Lighthouse", "Labs"]);
 
 
+describe("#tail", () => {
+  it("should return 2 elements", () => {
+    assert.StrictEqual(result.length, 2);
+  });
+  it("ensure first element is Lighthouse", () => {
+    assert.StrictEqual(result[0], "Lighthouse");
+  });
+});
 
-// Test Case 1: Check the returned array elements
+/* Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 console.log(assertEqual(result.length, 2)); // ensure we get back two elements
 console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
 console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+*/
